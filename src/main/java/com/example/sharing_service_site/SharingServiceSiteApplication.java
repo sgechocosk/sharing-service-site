133d0045ec7d516d57500f36cf8b572fdfd53cc0
Shunsuke Sugie <sunsk20tktk@gmail.com>
2025-08-31 00:50:47 +0900

    first commit

---
+++ src/main/java/com/example/sharing_service_site/SharingServiceSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.sharing_service_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharingServiceSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharingServiceSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sharing_service_site/SharingServiceSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sharing_service_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharingServiceSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
