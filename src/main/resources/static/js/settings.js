b00159c61e81115cb9d91dab53c40c7dbdf8f4e0
Shunsuke Sugie <sunsk20tktk@gmail.com>
2025-10-04 03:43:39 +0900

    トースト表示の実装

--- src/main/resources/static/js/settings.js
+++ src/main/resources/static/js/settings.js
@@ -1,19 +1,9 @@
-// document.querySelectorAll(".role-form").forEach((form) => {
-//   form.addEventListener("submit", async function (event) {
-//     event.preventDefault();
-
-//     const formData = new FormData(form);
-
-//     const response = await fetch(form.action, {
-//       method: "POST",
-//       body: formData,
-//     });
-
-//     if (response.ok) {
-//       const result = await response.json();
-//       alert(`ロールが「${result.newRoleName}」に変更されました。`);
-//     } else {
-//       alert("ロール変更に失敗しました。");
-//     }
-//   });
-// });
+window.addEventListener("DOMContentLoaded", () => {
+  const toast = document.getElementById("toast");
+  if (toast) {
+    toast.classList.add("show");
+    setTimeout(() => {
+      toast.classList.remove("show");
+    }, 3000);
+  }
+});
